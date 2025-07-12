1834f2f27aa28cfc83d75982282a7a3639329682
choudharymahipal <mschoudhary81@gmail.com>
2025-07-12 23:09:42 +0530

    version upgrade v18 to v19

--- src/app/Components/after-login/after-login.component.ts
+++ src/app/Components/after-login/after-login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-after-login',
-  templateUrl: './after-login.component.html',
-  styleUrl: './after-login.component.scss'
+    selector: 'app-after-login',
+    templateUrl: './after-login.component.html',
+    styleUrl: './after-login.component.scss',
+    standalone: false
 })
 export class AfterLoginComponent {
 
--- src/app/Components/before-login/before-login.component.ts
+++ src/app/Components/before-login/before-login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-before-login',
-  templateUrl: './before-login.component.html',
-  styleUrl: './before-login.component.scss'
+    selector: 'app-before-login',
+    templateUrl: './before-login.component.html',
+    styleUrl: './before-login.component.scss',
+    standalone: false
 })
 export class BeforeLoginComponent {
 
